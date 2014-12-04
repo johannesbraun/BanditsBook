bd31b2a2c7936c38a5f1fd1b61120ec385d133bb
JB <braun.johannes@gmail.com>
2014-12-04 16:47:32 +0100

    Adding a few comments and changing the path

--- r/epsilon_greedy/plot_standard.R
+++ r/epsilon_greedy/plot_standard.R
@@ -1,34 +1,46 @@
 library("plyr")
 library("ggplot2")
 
-results <- read.csv("julia/algorithms/epsilon_greedy/standard_results.csv", header = FALSE)
+filepath = "~/Mining-the-Social-Web-2nd-Edition/ipynb/algorithms/epsilon_greedy/"
+results <- read.csv(paste(filepath,"standard_results.tsv", sep=""), header = FALSE, sep = "\t")
+
 names(results) <- c("Epsilon", "Sim", "T", "ChosenArm", "Reward", "CumulativeReward")
 results <- transform(results, Epsilon = factor(Epsilon))
 
-# Plot average reward as a function of time.
+#head(results, n=100)
+#tail(results, n=100)
+#sapply(results, class)
+
+# Plot average reward (of all simulations) as a function of time.
 stats <- ddply(results,
                c("Epsilon", "T"),
                function (df) {mean(df$Reward)})
+#ddply: For each subset of a data frame, apply function then combine results into a data frame
+#equivalent of:
+#library("sqldf")
+#a<-sqldf("select Epsilon, T, avg(Reward) from results group by 1,2")
+
 ggplot(stats, aes(x = T, y = V1, group = Epsilon, color = Epsilon)) +
   geom_line() +
   ylim(0, 1) +
   xlab("Time") +
   ylab("Average Reward") +
   ggtitle("Performance of the Epsilon Greedy Algorithm")
-ggsave("r/graphs/standard_epsilon_greedy_average_reward.pdf")
+ggsave(paste(filepath,"/standard_epsilon_greedy_average_reward.pdf", sep=""))
 
 # Plot frequency of selecting correct arm as a function of time.
-# In this instance, 5 is the correct arm.
+# In this instance, 5 is the correct arm.  #
 stats <- ddply(results,
                c("Epsilon", "T"),
                function (df) {mean(df$ChosenArm == 5)})
+
 ggplot(stats, aes(x = T, y = V1, group = Epsilon, color = Epsilon)) +
   geom_line() +
   ylim(0, 1) +
   xlab("Time") +
   ylab("Probability of Selecting Best Arm") +
   ggtitle("Accuracy of the Epsilon Greedy Algorithm")
-ggsave("r/graphs/standard_epsilon_greedy_average_accuracy.pdf")
+ggsave(paste(filepath,"/standard_epsilon_greedy_average_accuracy.pdf", sep=""))
 
 # Plot variance of chosen arms as a function of time.
 stats <- ddply(results,
@@ -39,15 +51,16 @@ ggplot(stats, aes(x = T, y = V1, group = Epsilon, color = Epsilon)) +
   xlab("Time") +
   ylab("Variance of Chosen Arm") +
   ggtitle("Variability of the Epsilon Greedy Algorithm")
-ggsave("r/graphs/standard_epsilon_greedy_variance_choices.pdf")
+ggsave(paste(filepath,"/standard_epsilon_greedy_variance_choices.pdf", sep=""))
 
 # Plot cumulative reward as a function of time.
 stats <- ddply(results,
                c("Epsilon", "T"),
                function (df) {mean(df$CumulativeReward)})
+
 ggplot(stats, aes(x = T, y = V1, group = Epsilon, color = Epsilon)) +
   geom_line() +
   xlab("Time") +
   ylab("Cumulative Reward of Chosen Arm") +
   ggtitle("Cumulative Reward of the Epsilon Greedy Algorithm")
-ggsave("r/graphs/standard_epsilon_greedy_cumulative_reward.pdf")
+ggsave(paste(filepath,"/standard_epsilon_greedy_cumulative_reward.pdf", sep=""))
